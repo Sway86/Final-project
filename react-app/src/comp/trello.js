44e0ca695af3e7732b3ae554cc5cd887cd9ab5f3
Josue <josuep98@yahoo.com>
2019-02-07 20:05:52 -0500

    Third commit

--- react-app/src/comp/trello.js
+++ react-app/src/comp/trello.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { Board } from 'react-trello'
 
+function Person(name, address, phone) {
+    this.Name = name;
+    this.address = address;
+    this.phone = phone;
+
+    var person = [
+        "name",
+        "address",
+        "phone"
+    ]
+}
+
+
 function Trello() {
     function onDataChange() {
         console.log("I'm a changing...")
     }
+    function onCardDelete() {
+        <button onclick="function onCardDelete()"> Click me</button>
+
+        // <button onclick="function onCardDelete()"> Click me {
+        //     console.log("I'm deleting something")
+        // }
+        // </button>
+        // function onCardDelete() {
+        //     console.log("I'm deleteing something")
+    }
+    // function onCardAdd() {
+    //     console.log("I'm adding something")
+    // }
+    // function onCardClick() {
+    //     console.log("I'm clicking stuff")
+
 
     return (
 
@@ -12,32 +41,67 @@ function Trello() {
             data={{
                 lanes: [
                     {
-                        id: 'Card1',
-                        title: 'Waiting',
+                        id: 'Waiting for drop off',
+                        title: 'Drop Off',
                         cards: [
                             {
-                                id: 'Card1', title: 'Name: John Doe', description: 'Address: 901 Interntional Parkway LAke Mary', label: '30 mins'
+                                id: 'Waiting for drop off',
+                                title: 'Name: John Doe',
+                                description: 'Address: 901 Interntional Parkway Lake Mary',
+                                label: '30 mins'
+                            },
+                        ]
+
+                    },
+
+                    {
+                        id: 'Waitng for pick up',
+                        title: 'Pick Up',
+                        cards: [
+                            {
+                                id: 'Waitng for pick up',
+                                title: 'Name: Jeb Bush',
+                                description: 'Address: 1601 Pennsylvania Ave Washington',
+                                label: '2 years'
+                            },
+                        ]
+                    },
+
+                    {
+                        id: 'In transport',
+                        title: 'In transposrt',
+                        cards: [
+                            {
+                                id: 'In Transport',
+                                title: 'Name: Jane Doe',
+                                description: 'Address: 1099 Heathrow Park Lane',
+                                label: '30mins'
                             },
                         ]
 
                     },
                     {
-                        id: 'In transport',
-                        title: 'In transposrt',
-                        label: '10/20'
-
-                    },
-                    {
                         id: 'Done',
                         title: 'Done',
-                        label: '0/0'
+                        cards: [
+                            {
+                                id: 'Done',
+                                title: 'Name: El Presidente',
+                                description: 'Address: 1601 Pennsylvania Ave Washington',
+                                label: '2 years'
+                            }
+                        ]
 
                     },
 
                 ]
             }}
-            draggable
+            draggable laneDraggable={false}
+            id="EditableBoard1"
             onDataChange={onDataChange}
+            onCardDelete={onCardDelete}
+        // onCardAdd={onCardAdd}
+        // onCardClick={onCardClick}
         />
     )
 }
